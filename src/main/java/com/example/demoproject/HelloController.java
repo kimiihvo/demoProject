86b6f3679d5ef7545123e2ce920cc294123052bb
kimiihvo <kimiihvo@gmail.com>
2024-07-16 10:56:35 +0900

    first commit

---
+++ src/main/java/com/example/demoproject/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.demoproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "타임리프.............");
+        return "index";
+    }
+
+    @RequestMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello thymeleaf ~");
+        return "hello";
+    }
+
+    @RequestMapping("/main")
+    public String main(Model model) {
+        model.addAttribute("message", "타임리푸.");
+        return "main"; // "main"은 main.html 파일을 의미합니다.
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Title</title>
+</head>
+<body>
+<h1>이거 왜 안뜨지 밑에</h1>
+<h1>${message}</h1>
+</body>
+</html>
